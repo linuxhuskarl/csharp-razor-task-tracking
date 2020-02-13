7a4a09c213a60d13a662a615d0d503f193d58e18
Maciej Brzeski <linuxhuskarl@gmail.com>
2020-02-13 02:27:45 +0100

    Displaying Data on the Home Page

--- Tasker/Pages/Index.cshtml.cs
+++ Tasker/Pages/Index.cshtml.cs
@@ -5,13 +5,20 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Tasker
+namespace Tasker.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDbContext _context;
+
+        public IEnumerable<Task> Tasks { get; set; }
+        public IndexModel(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
         public void OnGet()
         {
-
+            Tasks = _context.Tasks.OrderBy(x => x.Priority);
         }
     }
 }
